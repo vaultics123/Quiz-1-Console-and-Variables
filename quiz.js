b4ba700d1975105a771ec15dbb64b5c36f8ae987
the vault <adrian.magmanlac@stu.ocsb.ca>
2024-03-25 13:39:01 -0400

    Update quiz.js

--- quiz.js
+++ quiz.js
@@ -12,24 +12,38 @@ console.log(t, "degrees Fahrenheit is equal to", c, "in degrees Celsius."); // O
 //Program 2: Grade Calculator
 let grade = prompt("Please enter your grade percentage this school year: "); // Gets grade from user and stores it in a variable called "grade"
 
-if (100 >= grade >= 90){
-    console.log("You have gotten an A this year! Congratulations!") // If the grade is between 90%-100%, this line is outputted and the user is given an A.
+if (grade >= 100){
+    console.log("This value is invalid.") // Outputs this message if user inputs a value greater than 100.
 }
-else if (80 <= grade >= 89){
-    console.log("You have gotten a B this year! You have reached good expectations.") // If the grade is between 80%-89%, this line is outputted and the user is given an B.
-}
-else if (70 <= grade >= 79){
-    console.log("You have gotten a C this year. You might have to work on your expectations for next year.") // If the grade is between 70%-79%, this line is outputted and the user is given an C.
-}
-else if (60 <= grade >= 69){
-    console.log("You have gotten a D this year. This is not good, but you still have a chance for next year.") // If the grade is between 60%-69%, this line is outputted and the user is given an D.
-}
-else if (0 <= grade >= 59){
-    console.log("You have gotten an F this year. This is very bad. You have basically failed.") // If the grade is below 60%, this line is outputted and the user is given an F.
+else if (grade < 0){
+    console.log("This value is invalid.") // Outputs this message if user inputs a value less than 0.
 }
 else{
-    console.log("Undefined.") // Outputs for negative values or values greater than 100.
+    if (grade >= 90){
+        console.log("You have gotten an A this year! Congratulations!") // If the grade is between 90%-100%, this line is outputted and the user is given an A.
+    }
+    else{
+        if (grade >= 80){
+            console.log("You have gotten a B this year! You have reached good expectations.") // If the grade is between 80%-89%, this line is outputted and the user is given an B.
+        }
+        else{
+            if (grade >= 70){
+                console.log("You have gotten a C this year. You might have to work on your expectations for next year.") // If the grade is between 70%-79%, this line is outputted and the user is given an C.
+            }
+            else{
+                if (grade >= 60){
+                    console.log("You have gotten a D this year. This is not good, but you still have a chance for next year.") // If the grade is between 60%-69%, this line is outputted and the user is given an D.
+                }
+                else{
+                    if (grade < 60){
+                        console.log("You have gotten an F this year. This is very bad. You have basically failed.") // If the grade is between 0%-59%, this line is outputted and the user is given an F.
+                    }
+                }
+            }
+        }
+    }
 }
+
 
 //Program 3: Leap Year Checker
 let year = prompt("Please enter the current year: ");
@@ -50,14 +64,14 @@ let num3 = prompt("Please enter the third random number: "); // Asks for the thi
 
 if (num1 > num2){
     if (num1 > num3){
-        console.log(num1, "is the greatest number.")
+        console.log(num1, "is the greatest number.") // Checks if num1 is bigger than both num2 and num3. If so, then num1 is the biggest number.
     }
 }
 else{
     if (num2 > num3){
-        console.log(num2, "is the greatest number.")
+        console.log(num2, "is the greatest number.") // If num1 is less than num2, it checks if num2 is bigger than num3, since num1 is less than num2. If so, num2 is the biggest number.
     }
     else{
-        console.log(num3, "is the greatest number.")
+        console.log(num3, "is the greatest number.") // If num1 is less than num3, it checks if num3 is bigger than num2, since num1 is less than num3. If so, num3 is the biggest number.
     }
 }
